f6d44e8c599c51c4a32c42c1958ad4a16434b728
CloudStrife <cloudandzack007@hotmail.com>
2025-05-28 13:01:47 +0800

    Update MessageBus.hpp
    保证线程安全

--- MessageBus.hpp
+++ MessageBus.hpp
@@ -1,6 +1,11 @@
 #pragma once
 #include <multidocument/DocumentContext.h>
 #include "MessageTypes.h"
+#include <QThread>
+#include <QPointer>
+#include <QCoreApplication>
+#include <QMutex>
+#include <QMutexLocker>
 #define MESSAGE_BUS_INSTANCE (MessageBus::instance())
 
 class MessageBus : public QObject
@@ -10,15 +15,36 @@ class MessageBus : public QObject
 public:
     static MessageBus* instance()
     {
-        static MessageBus instance;
-        return &instance;
+static QPointer<MessageBus> s_instance;
+
+        if (s_instance.isNull())
+        {
+            if (QThread::currentThread() != QCoreApplication::instance()->thread())
+            {
+                QMetaObject::invokeMethod(
+                    QCoreApplication::instance(),
+                    [&]()
+                    {
+                        if (s_instance.isNull())
+                        {
+                            s_instance = new MessageBus();
+                        }
+                    },
+                    Qt::BlockingQueuedConnection);
+            }
+            else
+            {
+                s_instance = new MessageBus();
+            }
+        }
+        return s_instance;
     }
 
     template <typename Receiver>
     void subscribe(Receiver* receiver, void (Receiver::*slot)(MessageType, const QVariant&, DocumentContext*))
     {
         static_assert(std::is_base_of<QObject, Receiver>::value, "Receiver must be a QObject or its derivative.");
-
+        QMutexLocker locker(&m_mutex);
         if (!connections.contains(receiver))
         {
             auto connection = connect(this, &MessageBus::messageSent, receiver, slot);
@@ -28,6 +54,7 @@ public:
 
     void unsubscribe(QObject* receiver)
     {
+        QMutexLocker locker(&m_mutex);
         if (connections.contains(receiver))
         {
             disconnect(connections[receiver]);
@@ -47,4 +74,5 @@ private:
     MessageBus(QObject* parent = nullptr) : QObject(parent) {}
     Q_DISABLE_COPY(MessageBus)
     QMap<QObject*, QMetaObject::Connection> connections;
+    QMutex m_mutex;
 };
